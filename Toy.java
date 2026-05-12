c4cd3fc837225e7585e27df58d8e7c358f557fbf
marcoyimyaem <marcoyimyaem@gmail.com>
2026-05-12 04:09:23 +0000

    Day 6

--- Toy.java
+++ Toy.java
@@ -1,12 +1,37 @@
+/**
+ * @author Marco Yimyaem
+ * Toy Object
+ * has a property of name, brand, price, and quantity
+ * whith a method of setPrice() where you can change 
+ * the price state of Toy Object.
+ */
+
 public class Toy {
-    public String name;
+    
+    public String name = "Gundam";
     public String brand;
     public double price;
     public int quantity;
+    /* this 
+    * is 
+    * a
+    * multi
+    * line
+    * comment
+    */
+
+
+   /*  set the amount of toy to the argument example setPrice(1000)*/
+    void setPrice(double price){
+        this.price = price;
+    }
     //psvm - public static void main(String[] args) {}
     public static void main(String[] args) {
         //sout System.out.println("hello java trainees");
         System.out.println("hello java trainees");
+        Toy toy1 = new Toy();
+        Toy toy2 = new Toy();
+        System.out.println(toy1.name);
     }
 
 }
