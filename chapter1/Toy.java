100f9651f271f88190577a9854843a78eae6e54d
marcoyimyaem <marcoyimyaem@gmail.com>
2026-05-13 04:12:00 +0000

    Day 7

--- chapter1/Toy.java
+++ chapter1/Toy.java
@@ -5,7 +5,7 @@
  * whith a method of setPrice() where you can change 
  * the price state of Toy Object.
  */
-
+package chapter1;
 public class Toy {
     
     public String name = "Gundam";
@@ -29,9 +29,28 @@ public class Toy {
     public static void main(String[] args) {
         //sout System.out.println("hello java trainees");
         System.out.println("hello java trainees");
-        Toy toy1 = new Toy();
+        Toy toy1 = new Toy() ;
+        toy1.brand="Lab Vuvu";
+        toy1.name="Rage Pink";
+        toy1.price=4500;
+        toy1.quantity=12;
         Toy toy2 = new Toy();
-        System.out.println(toy1.name);
+        toy1.setPrice(toy1.price*0.5);
+        System.out.println(toy1.price);
     }
 
+}
+class Fruit{}
+class Fruit2{}
+class Fruit3{
+    int numberOfLeaves;
+}
+class Fruit4{
+    public static void main(String[] args) {
+        System.out.println("I am Fruit 4");
+        Fruit3 f3 = new Fruit3();
+        f3.numberOfLeaves=31; 
+        System.out.println(f3.numberOfLeaves);
+        System.out.println(args[3]);
+    }
 }
