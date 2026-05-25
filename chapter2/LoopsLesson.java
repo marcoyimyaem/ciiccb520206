05e963607d36981658875dffb0308751f0d2eb96
marcoyimyaem <marcoyimyaem@gmail.com>
2026-05-25 03:59:59 +0000

    day 17

---
+++ chapter2/LoopsLesson.java
@@ -0,0 +1,11 @@
+package chapter2;
+
+public class LoopsLesson {
+    public static void main(String[] args) {
+        int x=0;
+        while(x<5)
+            System.out.println(x++);
+        System.out.println("endofprogram");
+        
+    }
+}
